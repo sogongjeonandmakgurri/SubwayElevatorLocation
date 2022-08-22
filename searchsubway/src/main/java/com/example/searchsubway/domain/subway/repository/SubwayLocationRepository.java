ef9a6e9eab4e939ece09127a03b4a4ffed9910a4
sooye7 <sooye7@naver.com>
2022-08-22 14:16:17 +0900

    FEAT: 근처 역 추천 기능 구현
    현재 위치의 근처에 있는 역 추천

--- searchsubway/src/main/java/com/example/searchsubway/domain/subway/repository/SubwayLocationRepository.java
+++ searchsubway/src/main/java/com/example/searchsubway/domain/subway/repository/SubwayLocationRepository.java
@@ -7,10 +7,10 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface SubwayLocationRepository extends JpaRepository<SubwayLocation,Long>, SubwayLocationCustomRepository {
-    @Query(value= "SELECT sub_name, ST_Distance_Sphere(POINT(:lng, :lat), POINT(lng, lat)) AS distance " +
+public interface SubwayLocationRepository extends JpaRepository<SubwayLocation,Long> {
+    @Query(value= "SELECT * " +
             "FROM sub_location " +
             "WHERE ST_Distance_Sphere(POINT(:lng, :lat), POINT(lng, lat)) <= 2000 " +
-            "ORDER BY distance", nativeQuery = true)
+            "ORDER BY ST_Distance_Sphere(POINT(:lng, :lat), POINT(lng, lat))", nativeQuery = true)
     public List<SubwayLocation> findCurrentSubwayStationByPosition( @Param("lng") Double lng, @Param("lat") Double lat);
 }
